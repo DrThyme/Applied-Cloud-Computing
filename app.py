5cb2bd47c0e91d612e802062f72640a4b31fa730
Tim Josefsson <tijo9673@student.uu.se>
2015-10-25 14:03:21 +0000

    fixing bugs

--- app.py
+++ app.py
@@ -1,49 +1,77 @@
-from celery import Celery
+from celery import Celery, group, subtask
 from pprint import pprint
 from flask import Flask, jsonify
+import sys
 import time
 import json
 import os
 import swiftclient.client
 
-config = {'username':os.environ['OS_USERNAME'], 
-          'api_key':os.environ['OS_PASSWORD'],
-          'project_id':os.environ['OS_TENANT_NAME'],
-          'auth_url':os.environ['OS_AUTH_URL']}
+app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'amqp://'
+app.config['CELERY_RESULT_BACKEND'] = 'amqp'
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
+
+config = {'user':os.environ['OS_USERNAME'], 
+          'key':os.environ['OS_PASSWORD'],
+          'tenant_name':os.environ['OS_TENANT_NAME'],
+          'authurl':os.environ['OS_AUTH_URL']}
 
 conn = swiftclient.client.Connection(auth_version=2, **config)
 
-app = Flask(__name__)
-worker = Celery(app.name, backend='amqp', broker='amqp://')
-
-master_count = {
-	'nr_of_tweets': 0,
-	'count_hon': 0,
-	'count_han':  0,
-	'count_den': 0,
-	'count_det': 0,
-	'count_denna': 0,
-	'count_denne': 0,
-	'count_hen': 0
-}
-
 def workerCount(tweets):
+	count = {
+		'#tweets': 0,
+		'hon': 0,
+		'han':  0,
+		'den': 0,
+		'det': 0,
+		'denna': 0,
+		'denne': 0,
+		'hen': 01
+	}
 	split_tweets = tweets.split('\n')
 	split_tweets = filter(None,split_tweets)
 	for tweet in split_tweets:
-		json_temp = json.load(item)
+		print tweet
+		json_temp = json.load(tweet)
 		if not json_temp['retweeted']:
-			master_count['nr_of_tweets'] += 1 
+			master_count['#tweets'] += 1 
 			check = json_temp['text'].lower() 
-			master_count['count_hon'] += check.count('hon') 
-			master_count['count_han'] += check.count('han') 
-			master_count['count_den'] += check.count('den') 
-			master_count['count_det'] += check.count('det') 
-			master_count['count_denna'] += check.count('denna') 
-			master_count['count_denne'] += check.count('denne') 
-			master_count['count_hen'] += check.count('hen')
+			count['hon'] += check.count('hon') 
+			count['han'] += check.count('han') 
+			count['den'] += check.count('den') 
+			count['det'] += check.count('det') 
+			count['denna'] += check.count('denna') 
+			count['denne'] += check.count('denne') 
+			count['hen'] += check.count('hen')
+	return(count)
 
-def countTweets(object_list):
+def sum_counts(master,results):
+	for result in results:
+		master['#tweets'] += result['#tweets']
+		master['hon'] += result['hon']
+		master['han'] += result['han']
+		master['den'] += result['den']
+		master['det'] += result['det']
+		master['denna'] += result['denna']
+		master['denne'] += result['denne']
+		master['hen'] += result['hen']
+	return(master)
+
+
+def countTweets():
+	(response, object_list) = conn.get_container('tweets')
+	master_count = {
+		'nr_of_tweets': 0,
+		'count_hon': 0,
+		'count_han':  0,
+		'count_den': 0,
+		'count_det': 0,
+		'count_denna': 0,
+		'count_denne': 0,
+		'count_hen': 0
+	}
 	for object in object_list:
 		(object_id,output) = conn.get_object("tweets", object['name'])
 		split_str = output.split('\n')
@@ -61,21 +89,30 @@ def countTweets(object_list):
 			master_count['count_hen'] += check.count('hen')
 		print("Object {} done!".format(object['name']))
 
-@worker.task
+@celery.task
 def check_tweet(object_name):
 	print "==== Worker fetching file:", str(object_name), "===="
-	(object_id,tweets) = conn.get_object("tweets", object_name['name'])
-	workerCount(tweets)
+	(object_id,output) = conn.get_object("tweets", object_name)
+	workerResult = workerCount(output)
 	print "==== Worker count on file:", str(object_name), "- COMPLETE! ===="
+	return(workerResult)
 
-@app.route('/', methods['GET'])
+@app.route('/', methods=['GET'])
 def display_info():
-	print "Welcome!"
-	print "In order to get to the application, please navigate to /run"
+	return "Welcome! In order to get to the application, please navigate to /run"
 
-@app.route('/run', methods['GET'])
+@app.route('/run', methods=['GET'])
 def allocate_tasks():
-	master_count = dict.fromkeys(master_count.iterkeys(),0)
+	master_count = {
+		'#tweets': 0,
+		'hon': 0,
+		'han':  0,
+		'den': 0,
+		'det': 0,
+		'denna': 0,
+		'denne': 0,
+		'hen': 0
+	}
 	(response, obj_list) = conn.get_container('tweets')
 	object_name_list = []
 	for object in obj_list:
@@ -86,11 +123,15 @@ def allocate_tasks():
 	results = t_group()
 
 	while not results.ready():
-		time.wait(4)
-		print "Wait for it!"
+		time.sleep(5)
+		completed = results.completed_count()
+		text = "\rProgress: {0}/20 subtasks done".format(completed) 
+		sys.stdout.write(text)
+		sys.stdout.flush()
 
-	print "ALL IS WELL!"
+	print "ALL SUBTASKS COMPLETE"
+	master_count = sum_counts(master_count,results.get())
 	return jsonify(master_count)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',debug=true)
+	app.run(host='0.0.0.0',debug=True)
