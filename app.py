13393650621bfd73741030f7228c4cd388790d81
Tim Josefsson <tijo9673@student.uu.se>
2015-10-25 14:21:05 +0000

    more bug fixes

--- app.py
+++ app.py
@@ -33,10 +33,9 @@ def workerCount(tweets):
 	split_tweets = tweets.split('\n')
 	split_tweets = filter(None,split_tweets)
 	for tweet in split_tweets:
-		print tweet
-		json_temp = json.load(tweet)
+		json_temp = json.loads(tweet)
 		if not json_temp['retweeted']:
-			master_count['#tweets'] += 1 
+			count['#tweets'] += 1 
 			check = json_temp['text'].lower() 
 			count['hon'] += check.count('hon') 
 			count['han'] += check.count('han') 
@@ -58,36 +57,6 @@ def sum_counts(master,results):
 		master['denne'] += result['denne']
 		master['hen'] += result['hen']
 	return(master)
-
-
-def countTweets():
-	(response, object_list) = conn.get_container('tweets')
-	master_count = {
-		'nr_of_tweets': 0,
-		'count_hon': 0,
-		'count_han':  0,
-		'count_den': 0,
-		'count_det': 0,
-		'count_denna': 0,
-		'count_denne': 0,
-		'count_hen': 0
-	}
-	for object in object_list:
-		(object_id,output) = conn.get_object("tweets", object['name'])
-		split_str = output.split('\n')
-		split_str = filter(None,split_str)
-		for item in split_str:
-			json_temp = json.loads(item)
-			master_count['nr_of_tweets'] += 1
-			check = json_temp['text'].lower()
-			master_count['count_hon'] += check.count('hon')
-			master_count['count_han'] += check.count('han')
-			master_count['count_den'] += check.count('den')
-			master_count['count_det'] += check.count('det')
-			master_count['count_denna'] += check.count('denna')
-			master_count['count_denne'] += check.count('denne')
-			master_count['count_hen'] += check.count('hen')
-		print("Object {} done!".format(object['name']))
 
 @celery.task
 def check_tweet(object_name):
